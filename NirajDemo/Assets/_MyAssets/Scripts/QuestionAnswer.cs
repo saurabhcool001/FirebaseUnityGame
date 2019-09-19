3254608749003d3f27a53f68744aeaa34659f749
saurabhcool001 <parmarsaurabh87@gmail.com>
2019-09-19 23:43:58 +0530

    Question Retrive From FIrebase
    This commit is shows the data retrive from firebase database and show in particular fields such as quesion field, options field, etc. Also, in this build, menubar sliding animation is included too but its not fully competed.

--- NirajDemo/Assets/_MyAssets/Scripts/QuestionAnswer.cs
+++ NirajDemo/Assets/_MyAssets/Scripts/QuestionAnswer.cs
@@ -2,15 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 using Firebase;
 using Firebase.Database;
 using Firebase.Unity.Editor;
+using TMPro;
 //using Firebase.Auth;
 
 public class QuestionAnswer : MonoBehaviour
 {
 
-    public Text usernameInput, passwordInput;
+    //public Text Q1Text, OpText1;
+
+    //public Text test;
+
+    public TextMeshProUGUI Q1Text, OpText1;
+
+    public string tempQueString, opt1String;
+
+    public Button submitBtn;
 
     //private Player data;
 
@@ -41,12 +51,29 @@ public class QuestionAnswer : MonoBehaviour
     //    databaseReference.Child("Users" + Random.Range(0, 100000)).SetRawJsonValueAsync(jsonData);
     //}
 
+    public void OnSelect(BaseEventData eventData)
+    {
+
+    }
+
+    public void TestSubmit()
+    {
+        Q1Text.text = tempQueString;
+        OpText1.text = opt1String;
+
+       
+    }
+
+
     public void LoadData()
     {
 
         FirebaseDatabase.DefaultInstance
             .GetReference("Questions")
             .GetValueAsync().ContinueWith(task => {
+
+                Debug.Log("Opened db");
+
                 if (task.IsFaulted)
                 {
                     print("Task is Faulted");
@@ -55,9 +82,18 @@ public class QuestionAnswer : MonoBehaviour
                 {
                     DataSnapshot dataSnapshot = task.Result;
                     string playerdata = dataSnapshot.Child("QID1").Child("question").GetRawJsonValue();
+
+                    Debug.Log("Data Retrieved");
+
+
+
+                    tempQueString = playerdata.ToString();
+                    //test.text = playerdata.ToString();
                     //print(playerdata);
                     string optionVal = dataSnapshot.Child("QID1").Child("option1").GetRawJsonValue();
                     print(playerdata + "___" + optionVal);
+
+                    opt1String = optionVal.ToString();
                 }
             });
         //FirebaseDatabase.DefaultInstance.GetReferenceFromUrl(DATA_URL)
